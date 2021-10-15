3608e5db665802d200fb29923c92ce7678ca03f4
Luke Mueller <lukemueller8@gmail.com>
2021-10-15 10:57:44 -0500

    moves js files into js dir

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-import mkLines from "./utils/mkLines.js";
-import mkRects from "./utils/mkRects.js";
-import setDimensions from "./utils/setDimensions.js";
+import mkLines from "./mkLines.js";
+import mkRects from "./mkRects.js";
+import setDimensions from "./setDimensions.js";
 
 const html = document.querySelector("html");
 const body = document.querySelector("body");
